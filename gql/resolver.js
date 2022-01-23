91fc73b0f7ff28dc045b8e153a4b7e57c9e1fe6b
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-01-22 21:58:20 -0600

    Playground funcionando en puerto 4000 mostrando usuario insertado en la consola

--- gql/resolver.js
+++ gql/resolver.js
@@ -1,8 +1,24 @@
+const User = require("../models/user");
+
+
 const resolvers = {
     Query: {
         // User
         getUser: ()=>{
             console.log("Obteniendo usuario");
+            return null;
+        }
+    },
+
+    Mutation: {
+        //User
+        register: (_, {input}) => {
+            const newUser = input;
+            newUser.email = newUser.email.toLowerCase();
+            newUser.username = newUser.username.toLowerCase();
+            
+
+            console.log(newUser);
             return null;
         }
     }
