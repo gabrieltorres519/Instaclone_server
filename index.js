fd31138339b419564b216297dd026d53d753ba7e
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-01-22 18:06:09 -0600

    subiendo los primeros archivos del servidor

---
+++ index.js
@@ -0,0 +1,35 @@
+const  mongoose = require("mongoose");
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./gql/schema");
+const resolvers = require("./gql/resolver");
+require("dotenv").config({ path: ".env"});
+
+
+
+
+mongoose.connect(process.env.BBDD,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, _)=> {
+    if(err){
+        console.log("Error de conexion");
+        console.log(err);
+    }else{
+       // console.log("Conexion correcta");
+       server();
+    }
+});
+
+function server() {
+    const serverApollo = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+
+    serverApollo.listen().then(({url})=>{
+        console.log("###################################");
+        console.log(`Servidor listo en la url ${url}`);
+        console.log("###################################");
+    });
+}
+
